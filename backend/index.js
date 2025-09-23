ce8b905ac03c8384c686b790f0bbdd4c9c1e003b
thanks299 <thanksagbeble@gmail.com>
2025-09-23 23:55:26 +0100

    fix: bcrypt error from bcrypt to bcryptjs

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const morgan = require('morgan');
